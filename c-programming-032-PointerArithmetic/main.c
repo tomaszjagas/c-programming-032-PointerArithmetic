ef4e2b6441e219d9924525943178875787282598
Tomasz Jagas <tomasz.jagas@gmail.com>
2021-01-02 21:46:52 +0100

    Add project files.

---
+++ c-programming-032-PointerArithmetic/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int arraySum(int array[], const int n);
+
+int main(void) {
+	int values[10] = { 3,7,-9,3,6,-1,7,9,1,-5 };
+
+	printf("The sum is %i\n", arraySum(values, 10));
+
+	return 0;
+}
+
+int arraySum(int array[], const int n) {
+	int sum = 0, * ptr;
+	int* const arrayEnd = array + n;
+
+	for (ptr = array; ptr < arrayEnd; ++ptr) {
+		sum += *ptr;
+	}
+
+	return sum;
+}
